240ea3c2db8117533ae62819b8f47376c800264a
Gustavo Chain <gchain@gmail.com>
2017-05-09 15:48:09 +0200

    Add OpenTracing hooks, closes #8

---
+++ hooks/othooks/othooks.go
@@ -0,0 +1,37 @@
+package othooks
+
+import "context"
+import "github.com/opentracing/opentracing-go"
+import "github.com/opentracing/opentracing-go/log"
+
+type Hook struct {
+	tracer opentracing.Tracer
+}
+
+func New(tracer opentracing.Tracer) *Hook {
+	return &Hook{tracer: tracer}
+}
+
+func (h *Hook) Before(ctx context.Context, query string, args ...interface{}) (context.Context, error) {
+	parent := opentracing.SpanFromContext(ctx)
+	if parent == nil {
+		return ctx, nil
+	}
+
+	span := h.tracer.StartSpan("sql", opentracing.ChildOf(parent.Context()))
+	span.LogFields(
+		log.String("query", query),
+		log.Object("args", args),
+	)
+
+	return opentracing.ContextWithSpan(ctx, span), nil
+}
+
+func (h *Hook) After(ctx context.Context, query string, args ...interface{}) (context.Context, error) {
+	span := opentracing.SpanFromContext(ctx)
+	if span != nil {
+		defer span.Finish()
+	}
+
+	return ctx, nil
+}
